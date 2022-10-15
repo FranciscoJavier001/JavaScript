38a106f433d4858d8fcdc0ea20dd6752bc335a23
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-10-14 22:45:51 -0500

    Declaration of Variables and Get Points by Teams

---
+++ 22_10_12-00-Code Challenge 3/main.js
@@ -0,0 +1,13 @@
+const delfines1 = prompt("Ingresa las anotaciones del 1er tiempo")
+const delfines2 = prompt("Ingresa las anotaciones del 2er tiempo")
+const delfines3 = prompt("Ingresa las anotaciones del 3er tiempo")
+
+const koalas1 = prompt("Ingresa las anotaciones del 1er tiempo")
+const koalas2 = prompt("Ingresa las anotaciones del 2er tiempo")
+const koalas3 = prompt("Ingresa las anotaciones del 3er tiempo")
+
+const delfinesTotal = (parseInt(delfines1)+parseInt(delfines2)+parseInt(delfines3))
+const koalasTotal = (parseInt(koalas1)+parseInt(koalas2)+parseInt(koalas3))
+
+console.log(delfinesTotal);
+console.log(koalasTotal);
