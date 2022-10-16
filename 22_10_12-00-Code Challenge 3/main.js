e49692147233a748cff41421a51e886f8aa63c4c
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-10-15 23:06:02 -0500

    Declarations Variables with Full Results

--- 22_10_12-00-Code Challenge 3/main.js
+++ 22_10_12-00-Code Challenge 3/main.js
@@ -1,13 +1,36 @@
-const delfines1 = prompt("Ingresa las anotaciones del 1er tiempo")
-const delfines2 = prompt("Ingresa las anotaciones del 2er tiempo")
-const delfines3 = prompt("Ingresa las anotaciones del 3er tiempo")
+// const delfines1 = prompt("Ingresa las anotaciones del 1er tiempo")
+// const delfines2 = prompt("Ingresa las anotaciones del 2er tiempo")
+// const delfines3 = prompt("Ingresa las anotaciones del 3er tiempo")
 
-const koalas1 = prompt("Ingresa las anotaciones del 1er tiempo")
-const koalas2 = prompt("Ingresa las anotaciones del 2er tiempo")
-const koalas3 = prompt("Ingresa las anotaciones del 3er tiempo")
+// const koalas1 = prompt("Ingresa las anotaciones del 1er tiempo")
+// const koalas2 = prompt("Ingresa las anotaciones del 2er tiempo")
+// const koalas3 = prompt("Ingresa las anotaciones del 3er tiempo")
 
-const delfinesTotal = (parseInt(delfines1)+parseInt(delfines2)+parseInt(delfines3))
-const koalasTotal = (parseInt(koalas1)+parseInt(koalas2)+parseInt(koalas3))
+// const delfinesTotal = (parseInt(delfines1)+parseInt(delfines2)+parseInt(delfines3))
+// const koalasTotal = (parseInt(koalas1)+parseInt(koalas2)+parseInt(koalas3))
 
-console.log(delfinesTotal);
-console.log(koalasTotal);
+// console.log(delfinesTotal);
+// console.log(koalasTotal);
+
+const Resultado1FinalDelfines = parseInt(293)
+const Resultado1FinalKoalas = parseInt(279)
+
+const Resultado2FinalDelfines = parseInt(316)
+const Resultado2FinalKoalas = parseInt(327)
+
+const Resultado3FinalDelfines = parseInt(310)
+const Resultado3FinalKoalas = parseInt(311)
+
+const Resultado4FinalDelfines = parseInt(312)
+const Resultado4FinalKoalas = parseInt(312)
+
+const nuevoResultado1Delfines = parseInt(Resultado1FinalDelfines / 3)
+const nuevoResultado2Delfines = parseInt(Resultado2FinalDelfines / 3)
+const nuevoResultado3Delfines = parseInt(Resultado3FinalDelfines / 3)
+
+const nuevoResultado1Koalas = parseInt(Resultado1FinalKoalas / 3)
+const nuevoResultado2Koalas = parseInt(Resultado2FinalKoalas / 3)
+const nuevoResultado3Koalas = parseInt(Resultado3FinalKoalas / 3)
+
+console.log(`#=${nuevoResultado1Delfines}, #=${nuevoResultado2Delfines}, #=${nuevoResultado3Delfines}`);
+console.log(`#=${nuevoResultado1Koalas}, #=${nuevoResultado2Koalas}, #=${nuevoResultado3Koalas}`);
