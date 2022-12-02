6092583ec253ee8a25ba3ee343d73d76d75c500c
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-12-01 22:19:53 -0600

    Code Challenge

---
+++ 22_10_28-02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,14 @@
+//** Reto de Codigo */
+
+/*
+Vamos a volver a Mark y John comparando sus IMC.
+Esta vez vamos a usar ibjetia para implementar el calculo
+Recuerda IMC = masa / altura ** 2 = masa / (peso * peso). Usa Km y Mts
+
+1.Para cada uno de ellos crea un ibjeto con propiedades como sus nombres completos, masa y altura Mark Miller y John Smith
+2.Crea un metodo de calcIMC en cada uno de los objetos a calcular el IMC, el mismo metodo en ambos objetos y wur rsn uns ptopiedad y lo retornen del metodo
+3.Muestra en onsola quien tiene un mayor IMC junto con su nombre completo y el del otro tambien
+
+Test Data: Marks weights 78 kg abnd is 1.69 m tall. John weights 92 kg and is 1.95 m tall
+*/
+
