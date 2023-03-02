17704eed12ca5e68a3b985bfc01e72febf8c6a3f
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-03-02 15:04:26 -0600

    Live-Server with Node

---
+++ 23-03-01-03-Developer-Skills/script.js
@@ -0,0 +1,10 @@
+// Remember, we're gonna use strict mode in all scripts now!
+'use strict';
+
+const x = '23';
+if (x === 23);
+console.log(23);
+
+const calcularEdad = (anoNacimiento) => 2023 - anoNacimiento;
+
+console.log(`Edad en 1987 = ${calcularEdad(1987)} años`);
