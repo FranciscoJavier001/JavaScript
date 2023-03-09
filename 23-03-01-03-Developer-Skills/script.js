1782cd33a31cf8b9424bab7ff1b4e2917100c61a
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-03-08 21:19:40 -0600

    Solved Full

--- 23-03-01-03-Developer-Skills/script.js
+++ 23-03-01-03-Developer-Skills/script.js
@@ -2,16 +2,22 @@
 
 //* Como se Programa */
 
-const arregloTemp = [-10, 5, 10, 'error']; //* Primero debemos de crear un arreglo de numeros */
+// const arregloTemp = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5]; //* Primero debemos de crear un arreglo de numeros */
 
-const calculoTemperaturas = (temperaturas) => {
-  //* En este caso debo de crar una funcion que reciba un parametro */
-  let min = temperaturas[0]; //* Debo de crear una variable que reciba un parametro del arreglo, en alguna posicion */
-  let max = temperaturas[0];
+const calculoTemperaturas = (t1, t2) => {
+  //* En este caso debo de crar una funcion que reciba dos parametro */
 
-  for (i = 0; i < temperaturas.length; i++) {
+  const temps = t1.concat(t2); //* Junto los 2 arrays en 1 solo */
+  console.log(temps); //* Muestro el arreglo nuevo completo */
+
+  let min = temps[0]; //* Debo de crear una variable que reciba un parametro del arreglo, en alguna posicion */
+  let max = temps[0];
+
+  for (i = 0; i < temps.length; i++) {
     //* Creo un ciclo que va a recorrer todo el arreglo creado */
-    const tempActual = temperaturas[i]; //* La tempActual esta en alguna variable de temperaturas */
+    const tempActual = temps[i]; //* La tempActual esta en alguna variable de temperaturas */
+
+    if (typeof tempActual !== 'number') continue; //* Reviso si la temperatura actual es diferente a un numero */
 
     if (tempActual < min) min = tempActual; //* Asigno temperatura minima y maxima */
     if (tempActual > max) max = tempActual;
@@ -19,4 +25,6 @@ const calculoTemperaturas = (temperaturas) => {
   console.log(min, max); //* Muestro en consola sus variables minimas y maximas */
   return max - min; //* Retorno el rango de temperaturas */
 };
-console.log(calculoTemperaturas(arregloTemp)); //* En consola invoco la funcion recibiendo un parametro */
+
+// console.log(calculoTemperaturas(arregloTemp)); //* En consola invoco la funcion recibiendo un parametro */
+console.log(calculoTemperaturas([-11, 19, 34], [-9, 40, 'error'])); //* Nuevas variables que recibe la funcion */
