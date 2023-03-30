e72ddb33a1656b4d165d0b3ea3b3d266cc33079f
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-03-29 23:24:32 -0600

    DOM Manipulation

--- 23_03_21-05-Guess-My-Number/script.js
+++ 23_03_21-05-Guess-My-Number/script.js
@@ -1,1 +1,4 @@
 'use strict'; //* Usamos el uso Estricto en JS */
+
+console.log(document.querySelector('.message')); //* Mostramos el objeto de la clase message */
+console.log(document.querySelector('.message').textContent); //* Mostramos una propiedad del objeto de la clase message */
