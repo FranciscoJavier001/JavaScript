096fde2d5dab9a3868104590283a7b20ac34dcfc
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-03-30 23:21:58 -0600

    DOM Manipulation

--- 23_03_21-05-Guess-My-Number/script.js
+++ 23_03_21-05-Guess-My-Number/script.js
@@ -2,3 +2,11 @@
 
 console.log(document.querySelector('.message')); //* Mostramos el objeto de la clase message */
 console.log(document.querySelector('.message').textContent); //* Mostramos una propiedad del objeto de la clase message */
+
+document.querySelector('.message').textContent = 'Numero Correcto'; //* Asi cambie lo que dice la clase message */
+
+document.querySelector('.number').textContent = 13; //* Cambie el valor de la clase number */
+document.querySelector('.score').textContent = 10; //* Cambie el valor de la clase score */
+
+document.querySelector('.guess').value = 13; //* Asi le asigno un valor */
+console.log(document.querySelector('.guess').value); //* Este valor no tengo nada, por eso no muestro nada en consola */
