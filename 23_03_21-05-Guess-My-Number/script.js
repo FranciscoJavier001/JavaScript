5ebb98e35d4953f6025f066d8a6efaecd2929226
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-04-06 23:52:38 -0600

    HighScore Implemented

--- 23_03_21-05-Guess-My-Number/script.js
+++ 23_03_21-05-Guess-My-Number/script.js
@@ -31,13 +31,17 @@ let score = 20; //* Valor inicial del score */
 
 // document.querySelector('.again').addEventListener('click', reloadPage); //* En la clase again, ponte lista del evento click y ejecuta funcion */
 
+let highscore = 0; //* Creamos una nueva variable que almacene el valor mas alto */
+
 document.querySelector('.again').addEventListener('click', function () {
   //* Cuando le de click al boton Reinicio */
+  score = 20; //* No entiendo porque tuve que definirlo aqui y no definirlo en la de abajo */
+
   secretNumber = Math.floor(Math.random() * 21); //* Asignamos un nuevo numero secreto */
   document.querySelector('.number').textContent = '?'; //* Cambiamos el texto a un ? */
   document.querySelector('.message').textContent = 'Comienza a Adivinar...'; //* Cambiamos el mensaje de texto */
   document.querySelector('body').style.backgroundColor = '#222'; //* Cambiamos el fondo de pantalla */
-  document.querySelector('.score').textContent = 20; //* Cambiamos el score */
+  document.querySelector('.score').textContent = score; //* Cambiamos el score */
   document.querySelector('.number').style.width = '15rem'; //* Cambio el tamaño del background donde tengo el ? */
   document.querySelector('.guess').value = ''; //* Asigamos un numero en blanco al campo de adivinar */
 });
@@ -54,8 +58,15 @@ document.querySelector('.check').addEventListener('click', function () {
     //* Cuando guess y el secretNumber sean iguales */
     document.querySelector('.message').textContent = '✅ Numero Correcto'; //* Muestra este mensaje en esta clase */
     document.querySelector('.number').textContent = guess; //* Coloca el numero secreto donde estaba el signo de ? */
+
     document.querySelector('body').style.backgroundColor = '#60b347'; //* Cuando le atine al color tambien cambio el background */
     document.querySelector('.number').style.width = '30rem'; //* Cambio el tamaño del background donde tengo el ? */
+
+    if (score > highscore) {
+      //* Si el score es mas alto que el highscore */
+      highscore = score; //* Asigna el highscore igual al score */
+      document.querySelector('.highscore').textContent = highscore; //* En esta clase, muestra el highscore con el score mas alto */
+    }
   } else if (guess > secretNumber) {
     //* Cuando guess sea mayor a secretNumber */
     document.querySelector('.message').textContent = '📈 Muy alto'; //* Muestra este mensaje en esta clase */
@@ -65,11 +76,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '📉 Muy bajo'; //* Muestra este mensaje en esta clase */
     score--; //* Disminuye el valor del score */
     document.querySelector('.score').textContent = score; //* Muestra este mensaje en esta clase */
+  }
 
-    if (score < 1) {
-      //* Cuando score sea 0 */
-      document.querySelector('.score').textContent = 'Perdiste'; //* Muestra este mensaje en esta clase */
-    }
+  if (score < 1) {
+    //* Cuando score sea 0 */
+    document.querySelector('.score').textContent = 'Perdiste'; //* Muestra este mensaje en esta clase */
   }
 });
 
