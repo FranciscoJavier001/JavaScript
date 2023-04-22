aa69200dba0de066b9f56d2433d49a2cb7d91230
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-04-21 19:05:32 -0600

    Define Initial Variables

--- 23_04_07-05-Duplicate-Adivina-Numero/script.js
+++ 23_04_07-05-Duplicate-Adivina-Numero/script.js
@@ -1,1 +1,5 @@
 "use strict"; //* Forzamos el uso estrictor de JS */
+
+let numeroSecreto = Math.floor(Math.random() * 21); //* Solicito un numero entero que este entre el 0 y el 20 */
+let puntaje = 20; //* Inicializamos el puntaje, importante definirlo aqui afuera */
+let puntajeMasAlto = 0; //* Inicializamos el puntaje, importante definirlo aqui afuera */
