daf0544b59a1cf037d1965235a2106c74dd62a03
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-04-21 19:08:15 -0600

    Define a function in a DOM with Actions

--- 23_04_07-05-Duplicate-Adivina-Numero/script.js
+++ 23_04_07-05-Duplicate-Adivina-Numero/script.js
@@ -3,3 +3,7 @@
 let numeroSecreto = Math.floor(Math.random() * 21); //* Solicito un numero entero que este entre el 0 y el 20 */
 let puntaje = 20; //* Inicializamos el puntaje, importante definirlo aqui afuera */
 let puntajeMasAlto = 0; //* Inicializamos el puntaje, importante definirlo aqui afuera */
+
+document.querySelector(".btnReinicio").addEventListener("click", function () {
+  //* En el DOM, seleccionamos una clase de una etiqueta button, le asignamos una accion click y que realice una funcion */
+});
