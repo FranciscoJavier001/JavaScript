13bc7d96b2413e1fb9ca1370b906b7934dcd25ba
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-04-22 20:58:38 -0600

    Updated Comment and Custom DOM width

--- 23_04_07-05-Duplicate-Adivina-Numero/script.js
+++ 23_04_07-05-Duplicate-Adivina-Numero/script.js
@@ -5,7 +5,7 @@ let puntaje = 20; //* Inicializamos el puntaje, importante definirlo aqui afuera
 let puntajeMasAlto = 0; //* Inicializamos el puntaje, importante definirlo aqui afuera */
 
 document.querySelector(".btnReinicio").addEventListener("click", function () {
-  //* En el DOM, seleccionamos una clase de una etiqueta button, le asignamos una accion click y que realice una funcion */
+  //* En el DOM, seleccionamos una clase de una etiqueta button, le asignamos una accion click y que realice una funcion boton Reinicio */
   puntaje = 20; //* Le asignamos un puntaje estatico */
 
   numeroSecreto = Math.floor(Math.random() * 21); //* Reasigno un nuevo numero Secreto del 0 al 20 */
@@ -13,4 +13,5 @@ document.querySelector(".btnReinicio").addEventListener("click", function () {
   document.querySelector(".mensaje").textContent = "Comienza a Adivinar..."; //* Cambiamos el mensaje */
   document.querySelector("body").style.backgroundColor = "#222"; //* Le pongo un color negro a todo el body */
   document.querySelector(".puntaje").textContent = puntaje; //* Voy a volver a colocar el puntaje en 20 */
+  document.querySelector(".numero").style.width = "15rem"; //* Le cambio el tamaño que tenia desde el inicio asignado  */
 });
