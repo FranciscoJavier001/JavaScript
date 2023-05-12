55b1011c2ea18ab618561828afb0abb52bb8e877
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-05-12 17:57:06 -0600

    Updated Selectors and Annotation

--- 23_05_06-06-Modal/script.js
+++ 23_05_06-06-Modal/script.js
@@ -3,4 +3,4 @@
 const modal = document.querySelector(".modal"); //* La variable modal, va a ser la clase modal del documento html */
 const superposicion = document.querySelector(".superposicion"); //* Le asigno esta clase del html a una variable */
 const btnCerrarModal = document.querySelector(".cerrar-modal"); //* Asigno una variable a una clase */
-const btnsMostrarModal = document.querySelector(".mostrar-modal"); //* Asigno una variable a una clase del html */
+const btnsMostrarModal = document.querySelectorAll(".mostrar-modal"); //* Asigno una variable a todas las clases del html,  */
