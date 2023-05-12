557415043326af59aae2a86e8361fefa0d4f5f8c
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-05-12 17:43:31 -0600

    Assign Variable to Class

--- 23_05_06-06-Modal/script.js
+++ 23_05_06-06-Modal/script.js
@@ -2,3 +2,4 @@
 
 const modal = document.querySelector(".modal"); //* La variable modal, va a ser la clase modal del documento html */
 const superposicion = document.querySelector(".superposicion"); //* Le asigno esta clase del html a una variable */
+const btnCerrarModal = document.querySelector(".cerrar-modal"); //* Asigno una variable a una clase */
