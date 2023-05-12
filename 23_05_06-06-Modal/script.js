8af16d10f28a7df6b8ddac87595390e073cffe91
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-05-12 17:57:47 -0600

    Show in Console All Modals

--- 23_05_06-06-Modal/script.js
+++ 23_05_06-06-Modal/script.js
@@ -4,3 +4,5 @@ const modal = document.querySelector(".modal"); //* La variable modal, va a ser 
 const superposicion = document.querySelector(".superposicion"); //* Le asigno esta clase del html a una variable */
 const btnCerrarModal = document.querySelector(".cerrar-modal"); //* Asigno una variable a una clase */
 const btnsMostrarModal = document.querySelectorAll(".mostrar-modal"); //* Asigno una variable a todas las clases del html,  */
+
+console.log(btnsMostrarModal); //* Mostramos en consola los Modales */
