8cf1017632605d3c70e29c58070fd607c86b2251
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-05-12 18:04:36 -0600

    For Loop to show all Modals in Console

--- 23_05_06-06-Modal/script.js
+++ 23_05_06-06-Modal/script.js
@@ -6,3 +6,8 @@ const btnCerrarModal = document.querySelector(".cerrar-modal"); //* Asigno una v
 const btnsMostrarModal = document.querySelectorAll(".mostrar-modal"); //* Asigno una variable a todas las clases del html,  */
 
 console.log(btnsMostrarModal); //* Mostramos en consola los Modales */
+
+for (let i = 0; i < btnsMostrarModal.length; i++) {
+  //* Recorro el arreglo de btnsMostrarModal con un for */
+  console.log(btnsMostrarModal[i].textContent); //* Muestro en consola cada uno de los elementos, con su contenido de texto */
+}
