1d0ff6c5686c637256970fc52cb45a1a6f25ac30
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-05-16 02:55:30 -0600

    Spell Correction

--- 23_05_06-06-Modal/script.js
+++ 23_05_06-06-Modal/script.js
@@ -3,7 +3,7 @@
 const modal = document.querySelector(".modal"); //* La variable modal, va a ser la clase modal del documento html */
 const superposicion = document.querySelector(".superposicion"); //* Le asigno esta clase del html a una variable */
 const btnCerrarModal = document.querySelector(".cerrar-modal"); //* Asigno una variable a una clase */
-const btnsMostrarModal = document.querySelectorAll(".mostrar-modal"); //* Asigno una variable a todas las clases del html,  */
+const btnsMostrarModal = document.querySelectorAll(".mostrar-modal"); //* Asigno una variable a todas las clases del html */
 
 console.log(btnsMostrarModal); //* Mostramos en consola los Modales */
 
@@ -17,7 +17,24 @@ for (let i = 0; i < btnsMostrarModal.length; i++) {
   });
 }
 
-btnCerrarModal.addEventListener("click", function () {
+// btnCerrarModal.addEventListener("click", function () {
+//   //* En este caso le damos la funcion al boton de cerrar */
+//   modal.classList.add("oculto"); //* Le volvemos a colocar esta clase */
+//   superposicion.classList.add("oculto"); //* Le vuelvo a dar esta clase */
+// });
+
+// superposicion.addEventListener("click", function () {
+//   //* Instruccion arriba -- "Debido a que se repite 2 veces la instruccion, busca implementarla solo 1 vez" */
+//   modal.classList.add("oculto");
+//   superposicion.classList.add("oculto");
+// });
+
+const cerrarModal = function () {
   modal.classList.add("oculto");
   superposicion.classList.add("oculto");
-});
+};
+
+btnCerrarModal.addEventListener("click", cerrarModal);
+
+// Como hacer para que donde le de click en la pantalla se salga
+//* Yo diria, que decirle en el body, agregue la clase cerrarModal */
