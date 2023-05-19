8398c1e2bd613d8889e6e95acc17c934b88dffd4
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-05-18 23:18:24 -0600

    Cleaner Code

--- 23_05_06-06-Modal/script.js
+++ 23_05_06-06-Modal/script.js
@@ -5,26 +5,20 @@ const superposicion = document.querySelector(".superposicion"); //* Le asigno es
 const btnCerrarModal = document.querySelector(".cerrar-modal"); //* Llamo a esta clase del html, personalizada en CSS */
 const btnsMostrarModal = document.querySelectorAll(".mostrar-modal"); //* Asigno una variable a todas las clases del html */
 
-console.log(btnsMostrarModal); //* Mostramos en consola los Modales */
-
+const abrirModal = () => {
+  modal.classList.remove("oculto"); //* De esta manera remuevo esa clase y se va a mostrar */
+  superposicion.classList.remove("oculto"); //* Entonces tambien le removemos la clase  */
+};
 for (let i = 0; i < btnsMostrarModal.length; i++) {
   //* Recorro el arreglo de btnsMostrarModal con un for */
-  btnsMostrarModal[i].addEventListener("click", function () {
-    //* A cada boton, con el evento click, lanzo funcion anonima que muestre en consola Click */
-    console.log("Click en Modal");
-    modal.classList.remove("oculto"); //* De esta manera remuevo esa clase y se va a mostrar */
-    superposicion.classList.remove("oculto"); //* Entonces tambien le removemos la clase  */
-  });
+  btnsMostrarModal[i].addEventListener("click", abrirModal); //* Cada ve que damos click en el Modal, evocamos la funcion */
 
-  btnCerrarModal.addEventListener("click", () => {
-    //* Nueva variable que es una funcion que obedece al evento click y llama a funcion */
+  const cerrarModal = () => {
+    //* Nueva variable, funcion de flecha que sigue instrucciones */
     modal.classList.add("oculto"); //* A la variable modal le volvemos a agregamos la clase oculto z-index */
     superposicion.classList.add("oculto"); //* Variable superposicion le volvemos a agregamos la clase oculto z-index */
-  });
+  };
 
-  superposicion.addEventListener("click", () => {
-    //* Variable, le asignamos un evento y le damos funcionalidades */
-    modal.classList.add("oculto"); //* A la variable modal le volvemos a agregamos la clase oculto z-index */
-    superposicion.classList.add("oculto"); //* Variable superposicion le volvemos a agregamos la clase oculto z-index */
-  });
+  btnCerrarModal.addEventListener("click", cerrarModal); //* Variable que esta en el DOM, le damos un evento y una funcion */
+  superposicion.addEventListener("click", cerrarModal); //* Variable que esta en el DOM, le damos un evento y una funcion */
 }
