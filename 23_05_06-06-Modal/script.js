0bdb9fdd70156f37aad11187d0c4d2877d7c4fc4
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-05-18 22:43:13 -0600

    New Variable with Class Access to Html and Css

--- 23_05_06-06-Modal/script.js
+++ 23_05_06-06-Modal/script.js
@@ -2,6 +2,7 @@
 
 const modal = document.querySelector(".modal"); //* La variable modal, va a ser la clase modal del documento html */
 const superposicion = document.querySelector(".superposicion"); //* Le asigno esta clase del html a una variable */
+const btnCerrarModal = document.querySelector(".cerrar-modal"); //* Llamo a esta clase del html, personalizada en CSS */
 const btnsMostrarModal = document.querySelectorAll(".mostrar-modal"); //* Asigno una variable a todas las clases del html */
 const reinicioDOM = document.querySelector(".reinicio"); //* Tomo control del DOM para reiniciarlo */
 
