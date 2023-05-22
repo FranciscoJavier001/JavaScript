8bea70a3073fbd3166e28ec097aaa9f3b094d265
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-05-21 20:20:42 -0600

    Function to Close Modal With Esc Key

--- 23_05_06-06-Modal/script.js
+++ 23_05_06-06-Modal/script.js
@@ -9,6 +9,7 @@ const abrirModal = () => {
   modal.classList.remove("oculto"); //* De esta manera remuevo esa clase y se va a mostrar */
   superposicion.classList.remove("oculto"); //* Entonces tambien le removemos la clase  */
 };
+
 for (let i = 0; i < btnsMostrarModal.length; i++) {
   //* Recorro el arreglo de btnsMostrarModal con un for */
   btnsMostrarModal[i].addEventListener("click", abrirModal); //* Cada ve que damos click en el Modal, evocamos la funcion */
@@ -26,4 +27,12 @@ superposicion.addEventListener("click", cerrarModal); //* Variable que esta en e
 document.addEventListener("keydown", (e) => {
   //* Agrego un evento al DOM, que sea al precionar una tecla reciba el evento que se lanzo */
   console.log(e.key); //* Muestro en consola el evento y muestro un atributo del evento */
+
+  if (e.key === "Escape") {
+    //* Creamos esta sentencia, si el evento en el atributo key es igual a la tecla Esc */
+    if (!modal.classList.contains("oculto")) {
+      //* Si en la variable modal, en su clase en su atributo contains lo tiene oculto */
+      cerrarModal(); //* Invoca la funcion de cerrarModal */
+    }
+  }
 });
