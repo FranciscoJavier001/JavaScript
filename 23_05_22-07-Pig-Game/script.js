96947c1bc547b2fa89a78ebfafab3bc3c4ed8edd
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-05-29 00:10:24 -0600

    New Variables in JS with Elements Classes Tags

--- 23_05_22-07-Pig-Game/script.js
+++ 23_05_22-07-Pig-Game/script.js
@@ -1,1 +1,5 @@
 "use strict";
+
+//* Seleccion de Elementos */
+const player0El = document.querySelector(".player--0"); //* Asignacion de Elemento en el DOM en Etiqueta Section */
+const player1El = document.querySelector(".player--º"); //* Asignacion de Elemento en el DOM en Etiqueta Section */
