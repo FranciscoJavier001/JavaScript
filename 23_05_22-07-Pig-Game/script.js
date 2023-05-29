bf673af9c633399ea60b243b90f7329d875f6990
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-05-29 00:25:36 -0600

    New Variables in JS with ID Elements in Tags

--- 23_05_22-07-Pig-Game/script.js
+++ 23_05_22-07-Pig-Game/script.js
@@ -3,3 +3,5 @@
 //* Seleccion de Elementos */
 const player0El = document.querySelector(".player--0"); //* Asignacion de Elemento en el DOM en Etiqueta Section */
 const player1El = document.querySelector(".player--1"); //* Asignacion de Elemento en el DOM en Etiqueta Section */
+const score0El = document.querySelector("#score--0"); //* Asignacion de Elemento en el DOM en Etiqueta P */
+const score1El = document.querySelector("#score--1"); //* Asignacion de Elemento en el DOM en Etiqueta P */
