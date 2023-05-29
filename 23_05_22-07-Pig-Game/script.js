6b7d0f82f80259e12c8cd4d3092c6c9df3945ef1
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-05-29 00:13:26 -0600

    Spell Correction

--- 23_05_22-07-Pig-Game/script.js
+++ 23_05_22-07-Pig-Game/script.js
@@ -2,4 +2,4 @@
 
 //* Seleccion de Elementos */
 const player0El = document.querySelector(".player--0"); //* Asignacion de Elemento en el DOM en Etiqueta Section */
-const player1El = document.querySelector(".player--º"); //* Asignacion de Elemento en el DOM en Etiqueta Section */
+const player1El = document.querySelector(".player--1"); //* Asignacion de Elemento en el DOM en Etiqueta Section */
