3b30f5fd3d34339e818c2587a7da10df935df52e
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-05-28 23:25:41 -0600

    Js File Working

--- 23_05_22-07-Pig-Game/script.js
+++ 23_05_22-07-Pig-Game/script.js
@@ -1,9 +1,1 @@
 "use strict";
-
-//* Seleccion de Elementos */
-const player0El = document.querySelector(); //* Aun no estan seleccionados en el DOM */
-const player1El = document.querySelector();
-const score0El = document.querySelector();
-const score1El = document.querySelector();
-const current0El = document.querySelector();
-const current1El = document.querySelector();
