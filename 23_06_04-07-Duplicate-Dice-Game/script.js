8f8437cd8fd7b40b3ed1f3d302a26d1811a1c728
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-06-08 16:47:46 -0600

    Declare Variables to Use and New Id Selectors

--- 23_06_04-07-Duplicate-Dice-Game/script.js
+++ 23_06_04-07-Duplicate-Dice-Game/script.js
@@ -3,6 +3,10 @@
 //* Seleccion de elementos */
 const jugador0 = document.querySelector(".jugador-0"); //* Selecciono <> por su clase */
 const jugador1 = document.querySelector(".jugador-1"); //* Selecciono <> por su clase */
+const puntos0 = document.querySelector("#puntos-0"); //* Selecciono <> por su id */
+const puntos1 = document.querySelector("#puntos-1"); //* Selecciono <> por su id */
+
+let puntos, puntosActuales; //* Empiezo a definir las variables para iniciar la funcion */
 
 jugador0.classList.remove(".jugador-ganador"); //* Cuando el Juego Empiece, les quitamos estas clases */
 jugador1.classList.remove(".jugador-ganador"); //* Cuando el Juego Empiece, les quitamos estas clases */
