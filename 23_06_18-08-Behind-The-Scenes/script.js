388018594db43f4d8da00bc9e61fac91fd7033da
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2023-06-19 21:38:19 -0600

    Script

--- 23_06_18-08-Behind-The-Scenes/script.js
+++ 23_06_18-08-Behind-The-Scenes/script.js
@@ -1,1 +1,39 @@
 "use strict";
+
+//* Alcance */
+
+const calcularEdad = (añoNacimiento) => {
+  //* Creo una funcion que recibe un parametro */
+  const edad = 2023 - añoNacimiento; //* Creo una variable que es una operacion aritmetica */
+
+  const imprimirEdad = () => {
+    //* Realizo una funcion, que no solicita nada */
+    let salidaConsola = `${primerNombre}, tu edad es de ${edad}, y naciste en ${añoNacimiento}`; //* Una variable, que reciba parametros */
+    console.log(salidaConsola); //* Muestro en consola el mensaje de esa variable */
+
+    if (añoNacimiento >= 1981 && añoNacimiento <= 1996) {
+      //* Condicion para esta funcion, como es cierta se lee */
+      var millenial = true; //* Tu que puedas cambiar, por ser un valor booleano */
+
+      const primerNombre = "Francisco"; //* Defino Primer Nombre, bajo el if */
+      salidaConsola = "Nueva Salida"; //* Defino un nuevo mensaje de esta variable */
+
+      const stringText = `Oh, tu eres un Millenial ${primerNombre}`; //* Variable, con un mensaje y variable aqui definida */
+      console.log(stringText); //* Hasta aqui llega el alcance de la variable */
+
+      const agregar = (a, b) => {
+        //* Nueva Funcion que recibe parametros para funcionar */
+        return a + b; //* Es lo que me retornan */
+      };
+      console.log(agregar(2, 3)); //* Mando llamar esta funcion y le paso parametros */
+    }
+
+    console.log(millenial); //* Hasta aqui llega esta variable */
+    console.log(salidaConsola); //* Hasta aqui llega esta variable */
+    console.log(edad); //* Hasta aqui llega esta variable */
+  };
+  imprimirEdad(); //* Mando llamar la funcion, para confirmar que sirva */
+};
+
+const primerNombre = "Jaime"; //* Esta es una variable Global */
+calcularEdad(1987); //* Funcion Global, parametro necesario*/
