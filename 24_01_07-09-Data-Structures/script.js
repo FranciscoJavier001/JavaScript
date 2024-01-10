f3791e58d5e381315a9b259471975bb0abe07fd4
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2024-01-09 23:14:02 -0600

    Annotations

--- 24_01_07-09-Data-Structures/script.js
+++ 24_01_07-09-Data-Structures/script.js
@@ -9,9 +9,11 @@ const restaurant = {
   menuPrincipal: ["Pizza", "Pasta", "Rioli"], //* Atributo dentro del objeto, es un arreglo */
 
   orden: function (pedidoInicial, pedidoPrincipal) {
+    //* Nueva variable ques una funcion que recibe 2 parametros */
     return [
-      this.menuInicial[pedidoInicial],
-      this.menuPrincipal[pedidoPrincipal],
+      //* Lo que regresa al ejecutarla */
+      this.menuInicial[pedidoInicial], //* El menu inicial se va a tomar como pedido inicial */
+      this.menuPrincipal[pedidoPrincipal], //* Menu Principal se va a tomar como pedido inicial */
     ];
   },
 };
@@ -28,13 +30,13 @@ console.log(arr); //* Muestro en consola el primer array que se llama arr */
 let [primero, segundo] = restaurant.categorias; //* Desestructuro el arreglo de este objeto y asigno ese orden */
 console.log(segundo, primero); //* Muestro en consola con el orden invertido */
 
-const [inicial, principal] = restaurant.orden(2, 0);
-console.log(inicial, principal);
+const [inicial, principal] = restaurant.orden(2, 0); //* Desestructuro 2 arreglos del objeto oeden de restaurant en su posicion 2 y 9 */
+console.log(inicial, principal); //* Mostramos estas 2 variables en consola */
 
-const anidado = [2, 4, [5, 6]];
+const anidado = [2, 4, [5, 6]]; //* Nueva variable que es un array anidado */
 // const [i, , j] = anidado;
-const [i, , [j, k]] = anidado;
-console.log(i, j, k);
+const [i, , [j, k]] = anidado; //* Defino la posicion de cada objeto en el arreglo anidado */
+console.log(i, j, k); //* Muestro en consola estas posiciones del arreglo */
 
-const [p = 1, q = 1, r = 1] = [8, 9];
-console.log(p, q, r);
+const [p = 1, q = 1, r = 1] = [8, 9]; //* Desestructuro el array y le asigno numeros pero solo cambia la ultima posicion */
+console.log(p, q, r); //* Muestro en consola el arreglo */
