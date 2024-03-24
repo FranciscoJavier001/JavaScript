8f940ebb52971cbcce9eab1203425aa7433190a5
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2024-03-23 20:33:11 -0600

    Sucks

--- 24_01_07-09-Data-Structures/script.js
+++ 24_01_07-09-Data-Structures/script.js
@@ -347,16 +347,35 @@
 // console.log(restaurante.size); //* Reviso el tamaño de la variable restaurante */
 // console.log(restaurante.clear); //* Limpio la informacion de esa variable */
 
-const eventoJuego = new Map([
-  [17, "⚽️ Gol"],
-  [(36, "🔁 Sustitucion")],
-  [(47, "⚽️ Gol")],
-  [(61, "🔁 Sustitucion")],
-  [(64, "🔸 Tarjeta")],
-  [(69, "🔴 Expulsado")],
-  [(70, "🔁 Sustitucion")],
-  [(72, "🔁 Sustitucion")],
-  [(76, "⚽️ Gol")],
-  [(80, "⚽️ Gol")],
-  [(92, "🔸 Tarjeta")],
-]);
+// const eventosJuego = new Map([
+//   [17, "⚽️ Gol"],
+//   [(36, "🔁 Sustitucion")],
+//   [(47, "⚽️ Gol")],
+//   [(61, "🔁 Sustitucion")],
+//   [(64, "🔸 Tarjeta")],
+//   [(69, "🔴 Expulsado")],
+//   [(70, "🔁 Sustitucion")],
+//   [(72, "🔁 Sustitucion")],
+//   [(76, "⚽️ Gol")],
+//   [(80, "⚽️ Gol")],
+//   [(92, "🔸 Tarjeta")],
+// ]);
+
+// const eventos = [...new Set(eventosJuego.values())];
+// console.log(eventos);
+
+// eventosJuego.delete(64);
+
+// console.log(
+//   `Un evento paso en aproximadamente, cada ${90 / eventosJuego.size} minutos`
+// );
+// const tiempo = [...eventosJuego.keys()].pop();
+// console.log(tiempo);
+// console.log(
+//   `Un evento ocurrio aproximadamente cada ${tiempo / eventosJuego.size} minutos`
+// );
+
+// for (const [min, evento] of eventosJuego) {
+//   const mitad = min <= 45 ? "Primero" : "Segundo";
+//   console.log(`[${mitad} Mitad] ${min}: ${evento}`);
+// }
