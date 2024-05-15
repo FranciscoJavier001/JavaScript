98086cba2e72211fdb6322f1484ff652b847a5e0
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2024-05-14 19:34:39 -0600

    Data Updated

--- 24_06_11-Data/data.js
+++ 24_06_11-Data/data.js
@@ -3,126 +3,265 @@
 😌Clientes😎
 
 🍀00-12-43-72-23-F8✅
-01/06 Sandoval Negocio 449.401.2419 - 01
+01/06 Sandoval Negocio 449.401.2419 - 01 / 0
+
+🍀00-F6-20-B8-2E-C5✅
+00/00 Texanox Tv 449.585.3603 - 01 / 0
+
+🍀02-15-20-63-AB-37✅
+01/06 Chiles Jefe 449.395.2155 - 01 / 100
+
+🍀06-1F-59-63-BF-D3✅
+01/06 Xochitl Ediel 449.537.9834 - 01 / 100
+
+🍀06-55-60-3B-29-03✅
+01/06 Claudia Edgar 449.752.1654 01 / 100
+
+🍀06-C2-9F-09-D5-3F✅
+01/06 Mariachis Ana 496.125.8920 - 01 / 100
+
+🍀0A-B0-9F-DA-04-C5✅
+00/00 Oscar Jefe 449.867.5205 - 01 / 0
+
+🍀1E-81-F4-C0-7B-EE✅
+01/05 Oscar Adrian 449.262.8169 - 01 / 100
+
+🍀22-1A-53-60-43-F3✅
+00/00 Texanox Lupita 449.524.1593 - 01 / 0
+
+🍀2A-10-A9-51-4A-08✅
+01/06 DosGarcia Calaca 449.197.5843 - 01 / 100
+
+🍀2A-F9-FB-1E-F1-25✅
+01/06 Gallegos Jefe 449.227.3349 - 01 / 100
+
+🍀32-ED-62-F8-48-17✅
+01/06 Johnny Enrique 449.805.4879 - 01 / 100
 
 🍀3C-52-A1-38-C6-C7✅
-01/06 Cariñosas Negocio XXX.XXX.XXXX - 01
+01/06 Cariñosas Negocio XXX.XXX.XXXX - 01 / 0
+
+🍀3C-52-A1-38-C7-7A✅
+01/06 China Negocio 449.548.4376 - 01 / 0
 
 🍀3C-52-A1-38-C8-B4✅
-01/06 Cholo Negocio XXX.XXX.XXXX - 01
+01/06 Cholo Negocio XXX.XXX.XXXX - 01 / 0
+
+🍀3C-52-A1-38-C8-DB✅
+01/06 Grillo Negocio 449.173.7770 - 01 / 0
 
 🍀3C-52-A1-38-C9-7D✅
-01/06 MariachisDos Negocio 449.106.1655 - 01
-
-🍀3C-52-A1-61-1A-09✅
-01/06 Cholo Negocio XXX.XXX.XXXX - 02
-
-🍀3C-52-A1-61-1A-AB✅
-01/06 Texanox Negocio 449.118.3135 - 01
-
-🍀3C-52-A1-61-1B-27✅
-01/06 DosGarcia Negocio XXX.XXX.XXXX - 01
-
-🍀3C-52-A1-61-1B-44✅
-01/06 Alvarado Negocio 449.243.1947 - 01
-
-🍀3C-52-A1-61-1C-F3✅
-01/06 Cuadros Negocio 449.186.5991 - 01
-
-🍀3C-52-A1-61-1D-89✅
-01/06 Quesos Negocio 449.406.2286 - 01
+01/06 MariachisDos Negocio 449.106.1655 - 01 / 0
 
 🍀3C-52-A1-61-12-BE✅
-01/06 MariachisTres Negocio 449.440.1177 - 01
+01/06 MariachisTres Negocio 449.440.1177 - 01 / 0
 
 🍀3C-52-A1-61-14-8F✅
-01/05 Juanita Negocio XXX.XXX.XXXX - 01
+01/05 Juanita Negocio XXX.XXX.XXXX - 01 / 0
 
 🍀3C-52-A1-61-14-9F✅
-01/06 TexanoDos Negocio 449.313.1264 - 01
+01/06 TexanoDos Negocio 449.313.1264 - 01 / 0
 
 🍀3C-52-A1-61-14-E9✅
-01/05 Johnny Negocio 449.218.8478 - 01
+01/05 Johnny Negocio 449.218.8478 - 01 / 0
 
 🍀3C-52-A1-61-15-D9✅
-01/06 Jitomates Negocio 449.313.1264 - 01
+01/06 Jitomates Negocio 449.313.1264 - 01 / 0
 
 🍀3C-52-A1-61-15-FD✅
-01/06 MariachisDos Negocio 449.590.4159 - 01
+01/06 MariachisDos Negocio 449.590.4159 - 01 / 0
 
 🍀3C-52-A1-61-16-3D✅
-01/06 Xochitl Negocio 449.119.4034 - 01
+01/06 Xochitl Negocio 449.119.4034 - 01 / 0
 
 🍀3C-52-A1-61-16-AD✅
-01/06 MariachisTres Negocio 449.440.1177 - 01
+01/06 MariachisTres Negocio 449.440.1177 - 01 / 0
 
 🍀3C-52-A1-61-17-A8✅
-01/06 Carmela Negocio 449.285.7396 - 01
+01/06 Carmela Negocio 449.285.7396 - 01 / 0
 
-🍀3C-52-A1-D9-4D-F7✅
-01/06 Carmela Negocio 449.285.7396 - 01
+🍀3C-52-A1-61-1A-09✅
+01/06 Cholo Negocio XXX.XXX.XXXX - 02 / 0
 
-🍀3C-52-A1-D9-4F-48✅
-01/06 Claudia Negocio 449.406.2286 - 01
+🍀3C-52-A1-61-1A-AB✅
+01/06 Texanox Negocio 449.118.3135 - 01 / 0
 
-🍀3C-52-A1-D9-5D-D8✅
-00/00 Oficina Camara 449.437.3323 - 01
+🍀3C-52-A1-61-1B-27✅
+01/06 DosGarcia Negocio XXX.XXX.XXXX - 01 / 0
 
-🍀3C-52-A1-D9-5D-FE✅
-01/06 DosGarcia Negocio XXX.XXX.XXXX - 02
+🍀3C-52-A1-61-1B-44✅
+01/06 Alvarado Negocio 449.243.1947 - 01 / 0
+
+🍀3C-52-A1-61-1C-F3✅
+01/06 Cuadros Negocio 449.186.5991 - 01 / 0
+
+🍀3C-52-A1-61-1D-89✅
+01/06 QuesosDos Negocio 449.406.2286 - 01 / 0
 
 🍀3C-52-A1-D9-40-B7✅
-01/06 Felipe Negocio XXX.XXX.XXXX - 01
+01/06 Felipe Negocio XXX.XXX.XXXX - 01 / 0
 
 🍀3C-52-A1-D9-40-D3✅
-01/06 Jitomates Negocio 449.313.1264 - 02
+01/06 Jitomates Negocio 449.313.1264 - 02 / 0
 
 🍀3C-52-A1-D9-42-86✅
-01/06 Jitomates Negocio 449.313.1264 - 03
+01/06 Jitomates Negocio 449.313.1264 - 03 / 0
+
+🍀3C-52-A1-D9-4D-F7✅
+01/06 Alvarado Negocio 449.243.1947 - 02 / 0
+
+🍀3C-52-A1-D9-4F-48✅
+01/06 Claudia Negocio 449.406.2286 - 01 / 0
 
 🍀3C-52-A1-D9-53-34✅
-01/06 Alvarado Negocio 449.243.1947 - 01
+01/06 Alvarado Negocio 449.243.1947 - 03 / 0
 
 🍀3C-52-A1-D9-54-6A✅
-00/00 Oficina Camara 449.437.3323 - 02
+00/00 Oficina Camara 449.437.3323 - 01 / 0
 
 🍀3C-52-A1-D9-57-89✅
-01/06 DosGarcia Negocio XXX.XXX.XXXX - 03
+01/06 DosGarcia Negocio XXX.XXX.XXXX - 02 / 0
 
 🍀3C-52-A1-D9-59-E1✅
-01/06 Johnny Negocio 449.218.8478 - 02
+01/06 Johnny Negocio 449.218.8478 - 02 / 0
 
 🍀3C-52-A1-D9-59-E2✅
-01/06 Chiles Negocio 449.395.2155 - 01
+01/06 Chiles Negocio 449.395.2155 - 01 / 0
+
+🍀3C-52-A1-D9-5D-D8✅
+00/00 Oficina Camara 449.437.3323 - 02 / 0
+
+🍀3C-52-A1-D9-5D-FE✅
+01/06 DosGarcia Negocio XXX.XXX.XXXX - 03 / 0
+
+🍀52-DD-45-30-68-3E✅
+00/00 Cristina Mudo 449.313.8608 - 01 / 0
+
+🍀5A-5B-84-76-81-3E✅
+01/06 Chiles Luis 449.189.3165 - 01 / 100
+
+🍀5A-F7-A6-EA-D2-D8✅
+01/06 Chata Tere 449.310.7924 - 01 / 100
 
 🍀5C-E9-31-40-37-BF✅
-01/06 Chiles Negocio 449.395.2155 - 02
+01/06 Chiles Negocio 449.395.2155 - 02 / 0
 
-🍀7C-24-99-5B-A6-D9✅
-00/00 Francisco Jefe 449.437.3323 - 01
+7C-24-99-5B-A6-D9
+00/00 Francisco Jefe 449.437.3323 - 01 / 0
+
+🍀7C-FD-6B-B3-D3-F5✅
+99/99 01/06 Alfredo Jefe 449.412.6234 - 02 / 100
+
+🍀7E-A0-F1-57-FB-D2✅
+01/06 Gorras Jose 449.530.3016 - 01 / 100
 
 🍀88-66-5A-49-8D-EE✅
-00/00 Francisco Jefe 449.437.3323 - 02
+00/00 Francisco Jefe 449.437.3323 - 02 / 0
 
 🍀90-6A-94-81-85-E4✅
-01/06 Mariachis Router 449.125.1437 - 01
+01/06 Mariachis Router 449.125.1437 - 01 / 0
 
 🍀96-67-B9-DD-53-8A✅
-00/00 Francisco Jefe 449.437.3323 - 03
+00/00 Francisco Jefe 449.437.3323 - 03 / 0
+
+🍀A2-53-71-DE-F5-5C✅
+01/06 Carmela Mari 449.285.7396 - 01 / 0
+
+🍀A2-98-19-99-BA-A6✅
+00/00 Texanox Gloria 449.142.4775 - 01 / 0
+
+🍀A2-F8-DF-7D-16-18✅
+01/06 Discos Marlon 449.434.2539 - 01 / 100
+
+🍀A6-13-C9-A0-15-A2✅
+00/00 Texanox Jefe 449.118.3135 - 01 / 0
 
 🍀A6-35-25-A9-D9-07✅
 99/99 01/06 phone
 
+🍀B4-74-43-76-D8-BF✅
+01/06 Punto Enrique XXX.XXX.XXXX - 01 / 0
+
+🍀B6-C7-BA-A0-C8-04✅
+01/06 China Jefe 449.284.4100 - 01 / 100
+
+🍀BC-7F-A4-37-B5-4F✅
+01/06 Gorras Jose 449.530.3016 - 02 / 100
+
+🍀BE-00-DC-76-19-56✅
+01/06 Gorras Ivanselmo 56.1501.2830 - 01 / 100
+
+🍀C6-53-63-27-DC-6D✅
+01/06 Gorras Ivanselmo 56.1501.2830 - 02 / 100
+
+🍀CA-AD-1A-E0-4B-65✅
+01/06 Alfredo Jefe 449.412.6224 - 01 / 100
+
 🍀CC-2D-21-B8-38-25✅
-01/06 Williams Router 449.502.4252 - 01
+01/06 Williams Router 449.502.4252 - 01 / 0
 
 🍀D6-14-F8-16-AE-92✅
-00/00 Francisco Jefe 449.437.3323 - 04
+00/00 Francisco Jefe 449.437.3323 - 04 / 0
+
+🍀E2-0D-09-1A-50-40✅
+01/06 Cholo Jefe 449.264.5451 - 01 / 100
+
+🍀E2-0F-FA-45-C3-1C✅
+01/06 Lechero Cristian 449.412.0419 - 01 / 100
 
 🍀F0-A7-31-58-44-25✅
-01/06 QuesosDos Negocio 449.406.2286 - 02
+01/06 QuesosDos Negocio 449.406.2286 - 01 / 0
 
 🍀F0-A7-31-58-6D-DB✅
-01/06 Colombiano Negocio 449.104.2698 - 01
+01/06 Colombiano Negocio 449.104.2698 - 01 / 0
+
+🍀F2-FB-AF-F7-7B-85✅
+01/06 Cigarros Fernanda 449.406.4030 - 01 / 100
+
+🍀FA-BF-EC-F6-61-60✅
+01/06 Lechero Jefe 449.461.9556 - 01 / 100
+
+🍀FE-68-17-CE-01-C1✅
+00/00 Texanox Joan 449.585.3603 - 01 / 0
 
 */
+
+
+// 
+// 
+// 
+46-72-74-3E-6D-51
+IPAD FRANCISCO
+
+56-5E-14-B0-58-4C
+01/06 Adolfo Eugenio 449.398.6064 - 01 / 100
+
+3E-74-CF-08-95-72
+01/06 Santiago Fernando 449.383.8358 - 01 / 100
+
+CE-B7-BF-C3-57-B7
+01/06 Chiles Daniela 449.107.4634 - 01 / 100
+
+4E-42-0E-90-43-C8
+01/06 Chiles Luis 449.189.3165 - 01 / 100
+
+🍀90-6A-94-81-85-E4✅
+01/06 Mariachis Router 449.125.1437 - 01 / 0
+
+82-C0-3C-06-CF-DB
+Beto Joseline 449.463.1699 
+
+EA-42-63-1F-F8-3B
+01/06 Charro Jefe (449.311.3421)
+
+86-A1-6B-5A-A3-43
+01/06 Charro Bruno (449.475.2068)
+
+46-64-aa-ed-b5-d7
+01/06 Jitomates Azael (449.549.3493)
+
+8E-58-33-33-FD-98
+00/00 Francisco Jefe 449.437.3323 - 01 / 0
+
+1A-90-9B-E6-C8-2B
+00/00 Francisco Jefe 449.437.3323 - 01 / 0
