ec75f4ac14382e083ef1b60b5a05122c8c807df6
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2024-06-16 11:49:56 -0600

    New Scripts

--- 24_06_15-Functions/script.js
+++ 24_06_15-Functions/script.js
@@ -24,3 +24,34 @@ createBooking("LH123", 5);
 
 createBooking("LH123", 1000);
 createBooking("LH123", undefined, 1000);
+
+const flight = "LH234";
+const jonas = {
+  name: "Jonas Schmedtmann",
+  passport: 24123123098,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = "Lh999";
+  passenger.name = "Mr. " + passenger.name;
+
+  if (passenger.passport === 24123123098) {
+    console.log("Check In");
+  } else {
+    console.log("Wrong Passport");
+  }
+};
+
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
+
+const flightNum = flight;
+const passenger = jonas;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
