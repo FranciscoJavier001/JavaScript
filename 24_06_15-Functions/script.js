67cec1b5471512dd5478f05c72389c8e90c05563
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2024-06-18 13:01:41 -0600

    New Scripts

--- 24_06_15-Functions/script.js
+++ 24_06_15-Functions/script.js
@@ -108,3 +108,53 @@
 // greeterHey("Steven"); //* Parametro que retorna la funcion de la linea 91 */
 
 // greet("Hello")("Jonas"); //* Invoco funcion greet y le paso los parametros que necesita */
+
+const lufthansa = {
+  airline: "Lufthansa",
+  iataCode: "Lh",
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(239, "Jonas Schmedtmann");
+lufthansa.book(635, "John Smith");
+console.log(lufthansa);
+
+const eurowings = {
+  airline: "Eurowings",
+  iataCode: "EW",
+  bookings: [],
+};
+
+const book = lufthansa.book;
+
+// Boes NOT Work
+// book(23, 'Sarah Williams')
+
+// Call Method
+book.call(eurowings, 23, "Sarah Williams");
+console.log(eurowings);
+
+book.call(lufthansa, 239, "Mary Cooper");
+console.log(lufthansa);
+
+const swiss = {
+  airline: "Swiss Air Lines",
+  iataCode: "LX",
+  bookings: [],
+};
+
+book.call(swiss, 583, "Mary Cooper");
+console.log(swiss);
+
+// Apply Method
+const flightData = [583, "George Cooper"];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+book.call(swiss, ...flightData);
