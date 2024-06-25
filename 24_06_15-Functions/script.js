cc8f42632451e45b85bb92e88a2b9d6573ea3abd
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2024-06-25 15:14:13 -0600

    Commented Code

--- 24_06_15-Functions/script.js
+++ 24_06_15-Functions/script.js
@@ -109,106 +109,106 @@
 
 // greet("Hello")("Jonas"); //* Invoco funcion greet y le paso los parametros que necesita */
 
-const lufthansa = {
-  //* Defino una variable que es un objeto */
-  airline: "Lufthansa", //* Le defino unas propiedades con informacio */
-  iataCode: "Lh",
-  bookings: [], //* Es un arreglo dentro de un objeto */
-  book(flightNum, name) {
-    //* Defino una funcion que recibe 2 parametros */
-    console.log(
-      //* Voy a mostrar en consola */
-      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
-    ); //* Scope con el name, el otro esta fuera, asi que utilizo el this */
-    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name }); //* En este arreglo, agrega esta informacion */
-  },
-};
+// const lufthansa = {
+//   //* Defino una variable que es un objeto */
+//   airline: "Lufthansa", //* Le defino unas propiedades con informacio */
+//   iataCode: "Lh",
+//   bookings: [], //* Es un arreglo dentro de un objeto */
+//   book(flightNum, name) {
+//     //* Defino una funcion que recibe 2 parametros */
+//     console.log(
+//       //* Voy a mostrar en consola */
+//       `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+//     ); //* Scope con el name, el otro esta fuera, asi que utilizo el this */
+//     this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name }); //* En este arreglo, agrega esta informacion */
+//   },
+// };
 
-lufthansa.book(239, "Jonas Schmedtmann"); //* Invoca esta funcion y llama esta funcion y pasale estos parametros */
-lufthansa.book(635, "John Smith"); //* Invoca esta funcion y llama esta funcion y pasale estos parametros */
-console.log(lufthansa); //* Muestra en consola estos parametros, son los primeros dos */
+// lufthansa.book(239, "Jonas Schmedtmann"); //* Invoca esta funcion y llama esta funcion y pasale estos parametros */
+// lufthansa.book(635, "John Smith"); //* Invoca esta funcion y llama esta funcion y pasale estos parametros */
+// console.log(lufthansa); //* Muestra en consola estos parametros, son los primeros dos */
 
-const eurowings = {
-  //* Defino una variable que dentro tiene propiedades */
-  airline: "Eurowings",
-  iataCode: "EW",
-  bookings: [],
-};
+// const eurowings = {
+//   //* Defino una variable que dentro tiene propiedades */
+//   airline: "Eurowings",
+//   iataCode: "EW",
+//   bookings: [],
+// };
 
-const book = lufthansa.book; //* Defino una variable, que haga el llamado a una funcion */
+// const book = lufthansa.book; //* Defino una variable, que haga el llamado a una funcion */
 
-// Boes NOT Work
-// book(23, 'Sarah Williams')
+// // Boes NOT Work
+// // book(23, 'Sarah Williams')
 
-// Call Method
-book.call(eurowings, 23, "Sarah Williams"); //* Llamo una funcion, con el metodo de llamada, llamo otra funcion y le paso parametros */
-console.log(eurowings); //* Muestro en consola lo que se recibio, es el la variable y sus propiedades como array */
+// // Call Method
+// book.call(eurowings, 23, "Sarah Williams"); //* Llamo una funcion, con el metodo de llamada, llamo otra funcion y le paso parametros */
+// console.log(eurowings); //* Muestro en consola lo que se recibio, es el la variable y sus propiedades como array */
 
-book.call(lufthansa, 239, "Mary Cooper"); //* Llamo la otra funcion pero con los mismos parametros */
-console.log(lufthansa); //* Lamo esta funcion y recibe estos parametros */
+// book.call(lufthansa, 239, "Mary Cooper"); //* Llamo la otra funcion pero con los mismos parametros */
+// console.log(lufthansa); //* Lamo esta funcion y recibe estos parametros */
 
-const swiss = {
-  //* Defino una variable y dentro le asigno propiedades */
-  airline: "Swiss Air Lines",
-  iataCode: "LX",
-  bookings: [],
-};
+// const swiss = {
+//   //* Defino una variable y dentro le asigno propiedades */
+//   airline: "Swiss Air Lines",
+//   iataCode: "LX",
+//   bookings: [],
+// };
 
-book.call(swiss, 583, "Mary Cooper"); //* Llamo la funcion y le paso parametros */
-console.log(swiss); //* Muestro en consola el arreglo definido */
+// book.call(swiss, 583, "Mary Cooper"); //* Llamo la funcion y le paso parametros */
+// console.log(swiss); //* Muestro en consola el arreglo definido */
 
-// Apply Method
-const flightData = [583, "George Cooper"]; //* Defino una variable que recibe un arreglo */
-book.apply(swiss, flightData); //* Llamo una funcion definida, y le paso los parametros de un arreglo */
-console.log(swiss); //* Muestro en consola lo que recibio el parametro */
+// // Apply Method
+// const flightData = [583, "George Cooper"]; //* Defino una variable que recibe un arreglo */
+// book.apply(swiss, flightData); //* Llamo una funcion definida, y le paso los parametros de un arreglo */
+// console.log(swiss); //* Muestro en consola lo que recibio el parametro */
 
-book.call(swiss, ...flightData); //* Llamo una fuincion, recibe un arreglo con propiedades, muestra la informacion dentro de la funcion */
+// book.call(swiss, ...flightData); //* Llamo una fuincion, recibe un arreglo con propiedades, muestra la informacion dentro de la funcion */
 
-const bookEW = book.bind(eurowings); //* Defino una variable y mando llamar la funcion */
-const bookLH = book.bind(lufthansa); //* Defino la variable y mando llamar la funcion */
-const bookLX = book.bind(swiss); //* Defino la variable y mando llamar la funcion */
+// const bookEW = book.bind(eurowings); //* Defino una variable y mando llamar la funcion */
+// const bookLH = book.bind(lufthansa); //* Defino la variable y mando llamar la funcion */
+// const bookLX = book.bind(swiss); //* Defino la variable y mando llamar la funcion */
 
-bookEW(23, "Steven Williams"); //* Invoco la funcion, le paso parametros que necesita y se muestra en consola porque asi defini la funcion */
+// bookEW(23, "Steven Williams"); //* Invoco la funcion, le paso parametros que necesita y se muestra en consola porque asi defini la funcion */
 
-const bookEW23 = book.bind(eurowings, 23); //* Defino una variable, que recibe parametros de una funcion con un argumento definido */
-bookEW23("Jonas Schmedtmann"); //* Le falta un arguento a la variable llamada, solo le falta el nombre */
-bookEW23("Martha Cooper");
+// const bookEW23 = book.bind(eurowings, 23); //* Defino una variable, que recibe parametros de una funcion con un argumento definido */
+// bookEW23("Jonas Schmedtmann"); //* Le falta un arguento a la variable llamada, solo le falta el nombre */
+// bookEW23("Martha Cooper");
 
-lufthansa.planes = 300; //* Le asigno un metodo llamado como planes a la variable lufthansa */
-lufthansa.buyPlane = function () {
-  //* Asigno un metodo que invoca una funcion */
-  //* Le asigno un */
-  console.log(this); //* Muestro lo que tenga este arreglo */
+// lufthansa.planes = 300; //* Le asigno un metodo llamado como planes a la variable lufthansa */
+// lufthansa.buyPlane = function () {
+//   //* Asigno un metodo que invoca una funcion */
+//   //* Le asigno un */
+//   console.log(this); //* Muestro lo que tenga este arreglo */
 
-  this.planes++; //* Le voy a agregar un valor a la variable que tiene el metodo planes y aumento 1 */
-  console.log(this.planes); //* Muestro en consola el nuevo valor de la suma en planes */
-};
+//   this.planes++; //* Le voy a agregar un valor a la variable que tiene el metodo planes y aumento 1 */
+//   console.log(this.planes); //* Muestro en consola el nuevo valor de la suma en planes */
+// };
 
-// lufthansa.buyPlane();
+// // lufthansa.buyPlane();
 
-document
-  .querySelector(".buy")
-  .addEventListener("click", lufthansa.buyPlane.bind(lufthansa));
-//* Selecciono en el documento la clase buy, cuando le haga click llama a este metodo, lo invocas como viene con el bind y lo vuelves a llamar */
+// document
+//   .querySelector(".buy")
+//   .addEventListener("click", lufthansa.buyPlane.bind(lufthansa));
+// //* Selecciono en el documento la clase buy, cuando le haga click llama a este metodo, lo invocas como viene con el bind y lo vuelves a llamar */
 
-const addTax = (rate, value) => value + value * rate; //* Defino una variable que recibe 2 valores, luego hace una operacion */
-console.log(addTax(0.1, 200)); //* Muestro en consola la funcion, recibiendo 2 prametros que se necesitan */
+// const addTax = (rate, value) => value + value * rate; //* Defino una variable que recibe 2 valores, luego hace una operacion */
+// console.log(addTax(0.1, 200)); //* Muestro en consola la funcion, recibiendo 2 prametros que se necesitan */
 
-const addVAT = addTax.bind(null, 0.23); //* Defino una variable que mando llamar una funcion, que viene con 2 valores */
-// addVAT = (value) => value + value * 0.23;
+// const addVAT = addTax.bind(null, 0.23); //* Defino una variable que mando llamar una funcion, que viene con 2 valores */
+// // addVAT = (value) => value + value * 0.23;
 
-console.log(addVAT(100)); //* Se le asigna un valor de 23% */
-console.log(addVAT(23)); //* Se le asigna un valor de 23% */
+// console.log(addVAT(100)); //* Se le asigna un valor de 23% */
+// console.log(addVAT(23)); //* Se le asigna un valor de 23% */
 
-const addTaxRate = function (rate) {
-  //* Declaro una variable que recibe un parametro */
-  return function (value) {
-    //* Retorno una nueva funcion que recibe un valor */
-    return value + value * rate; //* Esta me retorna una operacion aritmetica */
-  };
-};
+// const addTaxRate = function (rate) {
+//   //* Declaro una variable que recibe un parametro */
+//   return function (value) {
+//     //* Retorno una nueva funcion que recibe un valor */
+//     return value + value * rate; //* Esta me retorna una operacion aritmetica */
+//   };
+// };
 
-const addVAT2 = addTaxRate(0.23); //* Aqui le asigno un valor definido a la funcion declarada */
+// const addVAT2 = addTaxRate(0.23); //* Aqui le asigno un valor definido a la funcion declarada */
 
-console.log(addVAT2(100)); //* Se muestra el valor de la operacion pero con otra metodologia en declaracion de funcion */
-console.log(addVAT2(23));
+// console.log(addVAT2(100)); //* Se muestra el valor de la operacion pero con otra metodologia en declaracion de funcion */
+// console.log(addVAT2(23));
