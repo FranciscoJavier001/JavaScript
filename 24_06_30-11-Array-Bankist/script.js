3ab43e27149d755a0b76c99232362f7ddf1b4c44
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2024-07-17 14:40:40 -0600

    New Scripts

--- 24_06_30-11-Array-Bankist/script.js
+++ 24_06_30-11-Array-Bankist/script.js
@@ -305,3 +305,26 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300]; //* Un arreglo *
 // }, movements); //* De donde inicio el arreglo */
 
 // console.log(max); //* Muestro el mayor valor */
+
+const calcAverageHumanAge = (ages) => {
+  const humanAges = ages.map((age) => (age <= 2 ? 2 * age : 16 + age * 4));
+  const adults = humanAges.filter((age) => age >= 18);
+  console.log(humanAges);
+  console.log(adults);
+
+  // const average = adults.reduce((acc, age) => acc + age, 0) / adults.legth
+
+  const average = adults.reduce(
+    (acc, age, i, arr) => acc + age / arr.length,
+    0
+  );
+
+  // 2 3. (2+3)/2 = 2.5 === 2/2+3/2 = 2.5
+
+  return average;
+};
+
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+
+console.log(avg1, avg2);
