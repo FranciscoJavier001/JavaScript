3edcfcc6af1dd5476e2551b78b829efdbe8fff0f
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2024-07-31 21:37:47 -0600

    New Scripts

--- 24_06_30-11-Array-Bankist/script.js
+++ 24_06_30-11-Array-Bankist/script.js
@@ -621,3 +621,41 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300]; //* Un arreglo *
 //   const movementsUI2 = [...document.querySelectorAll(".movements__value")]; //* Variable, que recibe el arreglo de esta parte del documento */
 //   console.log(movementsUI2); //* Muestro en consola el array */
 // });
+
+const dogs = [
+  { weight: 22, curFood: 250, owners: ["Alice", "Bob"] },
+  { weight: 8, curFood: 200, owners: ["Matilda"] },
+  { weight: 13, curFood: 275, owners: ["Sarah", "John"] },
+  { weight: 32, curFood: 340, owners: ["Michael"] },
+];
+
+dogs.forEach((dog) => (dog.recFood = Math.trunc(dog.weight ** 0.75 * 28)));
+
+const dogSarah = dogs.find((dog) => dog.owners.includes("Sarah"));
+console.log(dogSarah);
+console.log(
+  `Sarah's dog is eating too ${
+    dogSarah.curFood > dogSarah.recFood ? "much" : "little"
+  }`
+);
+
+const ownersEatTooMuch = dogs
+  .filter((dog) => dog.curFood > dog.recFood)
+  .flatMap((dog) => dog.owners);
+
+console.log(ownersEatTooMuch);
+
+console.log(`${ownersEatTooMuch.join(" and ")}'s dogs eat too much`);
+console.log(`${ownersEatTooMuch.join(" and ")}'s dogs eat too little`);
+
+console.log(dogs.some((dog) => dog.curFood === dog.recFood));
+
+const checkEatingOkay = (dog) =>
+  dog.curFood > dog.recFood * 0.9 && dog.curFood < dog.recFood * 1.1;
+
+console.log(dogs.some(checkEatingOkay));
+
+console.log(dogs.filter(checkEatingOkay));
+
+const dogsSorted = dogs.slice().sort((a, b) => a.recFood - b.recFood);
+console.log(dogsSorted);
