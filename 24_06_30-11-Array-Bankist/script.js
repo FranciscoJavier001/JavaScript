94b9b5f26323983966acd6f518bf125882652292
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2024-09-09 18:42:49 -0600

    Commented Code and New Scripts

--- 24_06_30-11-Array-Bankist/script.js
+++ 24_06_30-11-Array-Bankist/script.js
@@ -8,9 +8,22 @@
 //* Arreglos */
 const account1 = {
   owner: "Jonas Schmedtmann",
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+
+  movementsDates: [
+    "2019-11-18T21:31:17.178Z",
+    "2019-12-23T07:42:02.383Z",
+    "2020-01-28T09:15:04.904Z",
+    "2020-04-01T10:17:24.185Z",
+    "2020-05-08T14:11:59.604Z",
+    "2019-05-27T17:01:17.194Z",
+    "2019-07-11T23:36:17.929Z",
+    "2019-07-12T10:51:36.790Z",
+  ],
+  currency: "EUR",
+  locale: "pt-PT",
 };
 
 const account2 = {
@@ -18,6 +31,19 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+
+  movementsDates: [
+    "2019-11-01T13:15:33.035Z",
+    "2019-11-30T09:48:16.867Z",
+    "2019-12-25T06:04:23.907Z",
+    "2020-01-25T14:18:46.235Z",
+    "2020-02-05T16:33:06.386Z",
+    "2020-04-10T14:43:26.374Z",
+    "2020-06-25T18:49:59.371Z",
+    "2020-07-26T12:01:20.894Z",
+  ],
+  currency: "USD",
+  locale: "en-US",
 };
 
 const account3 = {
@@ -150,6 +176,10 @@ const updateUI = (acc) => {
 };
 
 let currentAccount; //* Definimos una variable que puede cambiar */
+
+currentAccount = account1;
+updateUI(currentAccount);
+containerApp.style.opacity = 100;
 
 btnLogin.addEventListener("click", (e) => {
   //* Le damos el evento click a este elemento del dom y su argumento recibido es e para ponerle el preventDefault  */
@@ -752,21 +782,21 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300]; //* Un arreglo *
 // const huge = 202020202202n; //* Numero de tipo BigInt */
 // console.log(huge * BigInt(num)); //* Asi puedo definir un numero para hacer una operacion aritmetica */
 
-const now = new Date(); //* La fecha actual */
-console.log(now); //* Muestro en consola la fecha actual */
+// const now = new Date(); //* La fecha actual */
+// console.log(now); //* Muestro en consola la fecha actual */
 
-const future = new Date(2040, 1, 19, 15, 23, 50);
-console.log(future.getFullYear()); //* Año */
-console.log(future.getMonth()); //* Mes */
-console.log(future.getDate()); //* Dia */
-console.log(future.getDay()); //* Dia Semana*/
-console.log(future.getHours()); //* Hora */
-console.log(future.getMinutes()); //* Minutos */
-console.log(future.getSeconds()); //* Segundos */
-console.log(future.toISOString()); //* Muestro la hora en otro formato */
-console.log(future.getTime()); //* Muestro el tiempo  */
+// const future = new Date(2040, 1, 19, 15, 23, 50);
+// console.log(future.getFullYear()); //* Año */
+// console.log(future.getMonth()); //* Mes */
+// console.log(future.getDate()); //* Dia */
+// console.log(future.getDay()); //* Dia Semana*/
+// console.log(future.getHours()); //* Hora */
+// console.log(future.getMinutes()); //* Minutos */
+// console.log(future.getSeconds()); //* Segundos */
+// console.log(future.toISOString()); //* Muestro la hora en otro formato */
+// console.log(future.getTime()); //* Muestro el tiempo  */
 
-console.log(Date.now()); //* Fecha hoy */
+// console.log(Date.now()); //* Fecha hoy */
 
-future.setFullYear(2050); //* Cambio el año */
-console.log(future); //* Nueva fecha en consola */
+// future.setFullYear(2050); //* Cambio el año */
+// console.log(future); //* Nueva fecha en consola */
