6b2774db0c01c8dc960a4cda7c9ddc370ccd481f
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2024-12-26 07:36:49 -0600

    New Scripts and Annotations

--- 24_12_19-12-Scripts/script.js
+++ 24_12_19-12-Scripts/script.js
@@ -13,6 +13,14 @@ class Car { /* inicializo una clase, van con Mayusculas la primera */
         this.speed -= 5 /* Aterrizo esta operacion aritmetica y ocaciona un cambio aritmetico */
         console.log(`${this.brand} is going at ${this.speed} km/h`); /* Muestro en consola un mensaje */
     }
+
+    get speedUS() { /* Defino un una funcion */
+        return this.speed / 1.6 /* Retorno la propiedad interna con una operacion aritmetica */
+    }
+
+    set speedUSset({ speed }) { /* Metodo que redefine la funcion y recibe un argumento */
+        this.speed = speed * 1.6 /* Nuevo valor de Speed */
+    }
 }
 
 const bmw = new Car('BMW', 120) /* Creo un nuevo objeto llamado bmw que recibe parametros de la funcion */
@@ -25,4 +33,16 @@ bmw.accelerate()
 mercedes.accelerate()
 mercedes.accelerate()
 mercedes.brake()
-mercedes.accelerate() 
+mercedes.accelerate()
+
+const ford = new Car('Ford', 120) /* Inicializo una nueva variable con el costructor del carro y le paso argumentos */
+ford.accelerate() /* Invoco la funcion de acelerar */
+ford.accelerate()
+ford.accelerate()
+ford.brake() /* Invoco la funcion de frenado */
+console.log(ford.speedUS); /* Aqui tengo el resultado en consola de las millas por hora */
+
+ford.speedUSset = { speed: 500 } /* Reasigno el valor de speed */
+console.log(ford.speedUS); /* Muestro el nuevo valor de speed */
+
+/*  *//*  *//*  *//*  *//*  *//*  *//*  *//*  *//*  *//*  *//*  *//*  *//*  *//*  *//*  *//*  *//*  *//*  *//*  *//*  *//*  *//*  */
