44437766bdb0017f03c4fd978ac1cf8140d93a2a
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2025-04-14 19:08:34 -0600

    Remember ";"

--- 24_12_19-12-Scripts/script.js
+++ 24_12_19-12-Scripts/script.js
@@ -90,25 +90,85 @@
 // get3Countries('portugal', 'canada', 'tanzania')
 
 
+// const getJSON = function (url, errorMsg = 'Something went wrong') {
+//     return fetch(url).then(response => {
+//         if (!response.ok) throw new Error(`${errorMsg} (${response.status})`)
+
+//         return response.json()
+//     })
+// }
+
+// const get3Countries = async function (c1, c2, c3) {
+//     try {
+//         const data = await Promise.all([
+//             getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+//             getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+//             getJSON(`https://restcountries.com/v3.1/name/${c3}`)
+//         ])
+//         console.log(data.map(d => d[0].capital));
+//     } catch (err) {
+//         console.log(err);
+//     }
+// }
+
+// get3Countries('portugal', 'canada', 'tanzania')
+
+// const getJSON = function (url, errorMsg = 'Something went wrong') {
+//     return fetch(url).then(response => {
+//         if (!response.ok) throw new Error(`${errorMsg} (${response.status})`)
+//         return response.json()
+//     })
+// }
+
 const getJSON = function (url, errorMsg = 'Something went wrong') {
     return fetch(url).then(response => {
-        if (!response.ok) throw new Error(`${errorMsg} (${response.status})`)
+        if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+        return response.json();
+    })
+};
 
-        return response.json()
+    (async function () {
+        const res = await Promise.race([
+            getJSON(`https://restcountries.com/v3.1/name/italy`),
+            getJSON(`https://restcountries.com/v3.1/name/egypt`),
+            getJSON(`https://restcountries.com/v3.1/name/mexico`),
+        ])
+        console.log(res[0]);
+    })()
+
+const timeout = function (sec) {
+    return new Promise(function (_, reject) {
+        setTimeout(function () {
+            reject(new Error('Request took too long!'))
+        }, sec * 1000)
     })
 }
 
-const get3Countries = async function (c1, c2, c3) {
-    try {
-        const data = await Promise.all([
-            getJSON(`https://restcountries.com/v3.1/name/${c1}`),
-            getJSON(`https://restcountries.com/v3.1/name/${c2}`),
-            getJSON(`https://restcountries.com/v3.1/name/${c3}`)
-        ])
-        console.log(data.map(d => d[0].capital));
-    } catch (err) {
-        console.log(err);
-    }
-}
+Promise.race([
+    getJSON(`https://restcountries.com/v3.1/name/tanzania`),
+    timeout(5),
+])
+    .then(res => console.log(res[0]))
+    .catch(err => console.error(err))
 
-get3Countries('portugal', 'canada', 'tanzania')
+Promise.allSettled([
+    Promise.resolve('Success'),
+    Promise.reject('ERROR'),
+    Promise.resolve('Another Success')
+]).then(res => console.log(res))
+
+Promise.all([
+    Promise.resolve('Success'),
+    Promise.reject('ERROR'),
+    Promise.resolve('Another Success')
+])
+    .then(res => console.log(res))
+    .catch(err => console.error(err))
+
+Promise.any([
+    Promise.resolve('Success'),
+    Promise.reject('ERROR'),
+    Promise.resolve('Another Success')
+])
+    .then(res => console.log(res))
+    .catch(err => console.error(err))
